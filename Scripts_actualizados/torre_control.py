cdac77e64ed4c476177f6f394bc5c693bd23c9bf
Julián Fraga <60168622+julianfraga@users.noreply.github.com>
2022-11-03 16:53:57 -0300

    Una clase y una subclase tipo cola FIFO
    Defino la clase Cola y la subclase TorreDeControl que funciona con dos Colas en sí.
    
    Las colas son esencialmente listas con métodos que permiten agregar y desagregar elementos siguiendo la lógica "first in first out".
    
    TorreDeControl es una clase con una cola prioritaria (arribos) y una cola secundaria (partidas) representando aviones en el aires pidiendo pista para aterrizar (prioridad) y aviones en tierra pidiendo pistapara despegar

---
+++ Scripts_actualizados/torre_control.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 12:03:29 2022
+
+@author: Julián Fraga
+
+changelog 3/11:
+    + Agrego casteo a booleano en la clase Cola
+    + Modifico sintaxis obsoleta cambiando "not self.items.esta_vacia()" por 
+      su equivalente "bool(self.items)"
+"""
+
+class Cola:
+    '''Representa a una cola, con operaciones de encolar y desencolar.
+    El primero en ser encolado es tambien el primero en ser desencolado.
+    '''
+
+    def __init__(self, contenido_inicial=None):
+        '''Crea una cola vacia.'''
+        if contenido_inicial:
+            self.items=contenido_inicial
+        else:
+            self.items = []
+        
+
+    def encolar(self, x):
+        '''Encola el elemento x.'''
+        self.items.append(x)
+
+    def desencolar(self):
+        '''Elimina el primer elemento de la cola 
+        y devuelve su valor. 
+        Si la cola esta vacia, levanta ValueError.'''
+        if not self:
+            raise ValueError('La cola esta vacia')
+        return self.items.pop(0)
+
+    
+    # reemplazo por definición de casteo a booleano
+    # def esta_vacia(self):
+    #     '''Devuelve 
+    #     True si la cola esta vacia, 
+    #     False si no.'''
+    #     return len(self.items) == 0
+    
+    # Casteos
+    
+    def __str__(self):
+        return f'{self.items}'
+    
+    def __repr__(self):
+        return f'Cola({self.items})'
+    
+    def __bool__(self):
+        return len(self.items) != 0
+             
+
+
+class TorreDeControl:
+
+    def __init__(self, arribos_ini=None, partidas_ini=None):
+        '''Crea dos colas vacias.'''
+        self.arribos = Cola(arribos_ini)
+        
+        self.partidas = Cola(partidas_ini)
+      
+
+    def nuevo_arribo(self, avion_aire):
+        '''Agrega un avión al final de la cola arribos'''
+        self.arribos.encolar(avion_aire)
+
+        
+    def nueva_partida(self, avion_tierra):
+        '''Agrega un avión al final de la cola partidas'''
+        
+        self.partidas.encolar(avion_tierra)
+       
+    def ver_estado(self):
+        '''No anda todavía. Quiero que printee las dos listas separadas
+        por un salto de línea'''
+
+        print(f'Vuelos esperando para aterrizar: { ", ".join(self.arribos.items) }')
+        print(f'Vuelos esperando para despegar: { ", ".join(self.partidas.items) }')
+    
+    def asignar_pista(self):
+        '''Desencola arribos. Si no hay más arribos desencola partidas'''
+        if self.arribos:
+            vuelo=self.arribos.desencolar()
+            print(f'El vuelo {vuelo} aterrizó con éxito')
+            
+        elif self.partidas:
+            vuelo=self.partidas.desencolar()
+            print(f'El vuelo {vuelo} despegó')
+            
+        else:
+            print('No hay aviones en esperando pista')
+    
+    def __str__(self):
+        return(f'Arribos: {self.arribos} \nPartidas: {self.partidas}')
+    
+    def __repr__(self):
+        return f'TorreDeControl({self.arribos}, {self.partidas})'
+
+
